6f458a0263f02225b2791befa22b5510c3727ba5
Amir.mohamad <102855219+Amirmfth@users.noreply.github.com>
2026-09-24 15:23:48 +0330

    feat: add src/lib/format.ts

---
+++ src/lib/format.ts
@@ -0,0 +1,10 @@
+export function formatNumber(value: number) {
+  return new Intl.NumberFormat("en-US").format(value);
+}
+
+export function formatCompactNumber(value: number) {
+  return new Intl.NumberFormat("en-US", {
+    notation: "compact",
+    maximumFractionDigits: 1,
+  }).format(value);
+}
